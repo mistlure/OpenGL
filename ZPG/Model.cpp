237b68ba08adc571a6ce72a6e232dfae20cd6d0d
deenyse <denvery31@gmail.com>
2025-11-13 23:11:02 +0100

    Fix: scene_1 constructor

--- ZPG/Model.cpp
+++ ZPG/Model.cpp
@@ -2,37 +2,20 @@
 
 Model::Model(float* data, size_t size)
 {
-    // Generate VAO to store vertex attribute configuration
     glGenVertexArrays(1, &VAO);
-	// Generate VBO to store vertex data in GPU memory
-    glGenBuffers(1, &VBO);
-
-
+	glGenBuffers(1, &VBO);
 
     // Bind the VAO
-    // This stores the vertex attribute configuration
     glBindVertexArray(VAO);
 	// Bind the VBO
-    // This holds the actual vertex data
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     // Upload the vertex data to the GPU
-    // 'size' is the total byte size, 'data' is the pointer to the vertex array
     glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
 
-
-
-    // Enable vertex attribute at location 0
-    // This is typically used for vertex positions (vec3)
     glEnableVertexAttribArray(0);
-
-    // Define how attribute 0 is laid out in the buffer
-    // It reads 3 floats starting at offset 0, with a stride of 6 floats per vertex
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (GLvoid*)0);
 
 
-
-    // Enable vertex attribute at location 1
-    // This is typically used for normals or colors (vec3)
     glEnableVertexAttribArray(1);
     // Define how attribute 1 is laid out in the buffer
     // It reads 3 floats starting at offset 3 * sizeof(float), same stride
--- ZPG/Scene1.cpp
+++ ZPG/Scene1.cpp
@@ -6,16 +6,20 @@ Scene1::Scene1()
 {
 
     float triangle[] = {
-        // pos             // normal
-        -0.3f, -0.3f, 0.0f,  0.0f, 0.0f, 1.0f,
-         0.3f, -0.3f, -0.5f, 0.0f, 0.0f, 1.0f,
-         0.0f,  0.3f, 0.0f,  0.0f, 0.0f, 1.0f
+        1.0f,  0.0f,  1.0f, 0.0f, 1.0f, 0.0f,
+        1.0f,  0.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+        -1.0f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f,
     };
 
     Model* triangleModel = new Model(triangle, sizeof(triangle));
 
 	DrawableObject* triangleObject = new DrawableObject(ShaderPairs::Phong, triangleModel, getCamera(), light);
-	triangleObject->transform->addTransform(new Translate(glm::vec3(0.0f, 0.0f, -1.0f)));
+
+
+    addObject(triangleObject);
+
+    triangleObject = new DrawableObject(ShaderPairs::Phong, triangleModel, getCamera(), light);
+    triangleObject->transform->addTransform(new Translate(glm::vec3(0.0f, 1.0f, -2.0f)));
 
     addObject(triangleObject);
 
