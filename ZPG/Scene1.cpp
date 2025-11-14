6c5dbd24b0f9a7f6cd564285e7cdc1eac3ba096e
deenyse <denvery31@gmail.com>
2025-11-14 20:27:46 +0100

    Add: test scene-1

--- ZPG/Scene1.cpp
+++ ZPG/Scene1.cpp
@@ -6,25 +6,30 @@ Scene1::Scene1()
 {
 
 	addLight(new PointLight(glm::vec3(0.0f, 5.0f, 5.0f), glm::vec3(1.f)));
-	addLight(new PointLight(glm::vec3(0.0f, -5.0f, -5.0f), glm::vec3(1.f)));
-    float triangle[] = {
-        1.0f,  0.0f,  1.0f, 0.0f, 1.0f, 0.0f,
-        1.0f,  0.0f, -1.0f, 0.0f, 1.0f, 0.0f,
-        -1.0f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f,
-    };
+	addLight(new PointLight(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.f)));
 
-    Model* triangleModel = new Model(sphere, sphereSize);
-
-	DrawableObject* triangleObject = new DrawableObject(ShaderPairs::Phong, triangleModel, getCamera(), &lights);
-
-
-    addObject(triangleObject);
-
+	// Create a single sphere model
 	Model* sphereModel = new Model(sphere, sphereSize);
-    triangleObject = new DrawableObject(ShaderPairs::Phong, sphereModel, getCamera(), &lights);
-    triangleObject->transform->addTransform(new Translate(glm::vec3(0.0f, 1.0f, -2.0f)));
 
-    addObject(triangleObject);
+	// First sphere
+	DrawableObject* sphere1 = new DrawableObject(ShaderPairs::Phong, sphereModel, getCamera(), &lights);
+	sphere1->transform->addTransform(new Translate(glm::vec3(-2.0f, -0.0f, 0.0f)));
+	addObject(sphere1);
+
+	// Second sphere
+	DrawableObject* sphere2 = new DrawableObject(ShaderPairs::Phong, sphereModel, getCamera(), &lights);
+	sphere2->transform->addTransform(new Translate(glm::vec3(0.0f, -2.0f, 0.0f)));
+	addObject(sphere2);
+
+	// Third sphere
+	DrawableObject* sphere3 = new DrawableObject(ShaderPairs::Phong, sphereModel, getCamera(), &lights);
+	sphere3->transform->addTransform(new Translate(glm::vec3(0.0f, 2.0f, 0.0f)));
+	addObject(sphere3);
+
+	// Fourth sphere
+	DrawableObject* sphere4 = new DrawableObject(ShaderPairs::Phong, sphereModel, getCamera(), &lights);
+	sphere4->transform->addTransform(new Translate(glm::vec3(2.0f, 0.0f, 0.0f)));
+	addObject(sphere4);
 
 
 }
